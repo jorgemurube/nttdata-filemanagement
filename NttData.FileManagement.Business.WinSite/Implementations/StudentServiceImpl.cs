02fa14aa5bb1fd87ae3144966ec3674bbb2c0370
jmurubes <jmurube@emeal.nttdata.com>
2023-01-17 13:22:00 +0100

    Added student scaffolding implementation with student classes in all layers closes #3

---
+++ NttData.FileManagement.Business.WinSite/Implementations/StudentServiceImpl.cs
@@ -0,0 +1,18 @@
+﻿using NttData.FileManagement.Business.WinSite.Contracts;
+using NttData.FileManagement.Common.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NttData.FileManagement.Business.WinSite.Implementations
+{
+    public class StudentServiceImpl : IStudentService
+    {
+        public bool Add(Student student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NttData.FileManagement.DataAcces.Repository/Implementations/StudentRepositoryImpl.cs
@@ -0,0 +1,18 @@
+﻿using NttData.FileManagement.Common.Model;
+using NttData.FileManagement.DataAcces.Repository.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NttData.FileManagement.DataAcces.Repository.Implementations
+{
+    public class StudentRepositoryImpl : IStudentRepository
+    {
+        public bool Add(Student student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
